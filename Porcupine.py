aed2b666489adfa3f33dee3516f7e4e35a104e79
fuff0l0 <hmorikova@aol.com>
2013-02-06 01:29:41 +0100

    Initial commit

---
+++ Porcupine.py
@@ -0,0 +1,148 @@
+#!/usr/bin/python
+
+import wx
+from wx import xrc
+import subprocess
+from pyudev import Context, Monitor
+import pyudev.wx
+
+
+class Porcupine_main(wx.Frame):
+    def __init__(self):
+        self.InitUI()
+        self.observer_status = 0
+        self.obs = Observer(self.radio_mode, self.chkbox_usb, self.chkbox_cd, self.chkbox_sd)
+        self.tskic = PorcupineTaskBarIcon(self)
+
+    def InitUI(self):
+        self.ui = xrc.XmlResource('Porcupine.xrc')
+        self.frame_settings = self.ui.LoadFrame(None, 'Porcupine_settings')
+        
+        self.radio_mode = xrc.XRCCTRL(self.frame_settings, "radiobox_mode")
+        self.chkbox_usb = xrc.XRCCTRL(self.frame_settings, "chkbox_usb")
+        self.chkbox_cd = xrc.XRCCTRL(self.frame_settings, "chkbox_cd")
+        self.chkbox_sd = xrc.XRCCTRL(self.frame_settings, "chkbox_sd")
+        self.frame_settings.Bind(wx.EVT_CLOSE, self.FrameClose)
+
+    def check_usb():
+        pass
+
+    def OnAbout(self, event):
+        wx.MessageBox("ABOUT.")
+
+    def OnQuit(self, event):
+        self.tskic.Destroy()
+        if self.frame_settings:
+            self.frame_settings.Destroy()
+
+    def FrameClose(self, event):
+        self.frame_settings.Hide()
+
+    def OnSettings(self, event):
+        if not self.frame_settings:
+            self.frame_settings = self.ui.LoadFrame(None, 'Porcupine_settings')
+        self.frame_settings.Show()
+
+    def OnObserverStart(self, event):
+    	if(self.observer_status == 0):
+            self.obs.OnStart()
+            self.observer_status = 1
+        elif(self.observer_status == 1):
+            pass
+        else:
+            pass
+
+    def OnObserverStop(self, event):
+        if(self.observer_status == 1):
+            self.obs.OnStop()
+            self.observer_status = 0
+        elif(self.observer_status == 0):
+            pass
+        else:
+            pass
+
+
+class PorcupineTaskBarIcon(wx.TaskBarIcon):
+    def __init__(self, frame):
+        wx.TaskBarIcon.__init__(self)
+        self.frame = frame
+        self.SetIcon(wx.Icon('porcupine_icon.png', wx.BITMAP_TYPE_PNG), 'Porcupine')
+        self.Bind(wx.EVT_MENU, self.frame.OnObserverStart, id=1)
+        self.Bind(wx.EVT_MENU, self.frame.OnObserverStop, id=2)
+        self.Bind(wx.EVT_MENU, self.frame.OnSettings, id=3)
+        self.Bind(wx.EVT_MENU, self.frame.OnAbout, id=4)
+        self.Bind(wx.EVT_MENU, self.frame.OnQuit, id=5)
+
+    def CreatePopupMenu(self):
+        self.menu = wx.Menu()
+        self.menu.Append(1, 'Start')
+        self.menu.Append(2, 'Stop')
+        self.menu.Append(3, 'Settings')
+        self.menu.Append(4, 'About')
+        self.menu.Append(5, 'Quit')
+        return self.menu
+
+
+class Observer:
+    def __init__(self, radio_mode, chkbox_usb, chkbox_cd, chkbox_sd):
+        self.context = Context()
+        self.evil = Evil(self)
+        self.radio_mode = radio_mode
+        self.chkbox_usb = chkbox_usb
+        self.chkbox_cd = chkbox_cd
+        self.chkbox_sd = chkbox_sd
+
+    def OnStart(self):
+        self.monitor = Monitor.from_netlink(self.context)
+        self.monitor.filter_by(subsystem='block')
+        self.obs = pyudev.wx.WxUDevMonitorObserver(self.monitor)
+        self.obs.Bind(pyudev.wx.EVT_DEVICE_EVENT, self.OnDispatch)
+        self.monitor.start()
+
+    def OnStop(self):
+        self.obs.enabled = False
+
+    def OnDispatch(self, event):
+        if(str(event.action) == "add" and str(event.device.device_node[7]) != "a" and str(event.device.device_node[5:7]) == "sd" and self.chkbox_usb.GetValue() == True):
+            if(self.radio_mode.GetStringSelection() == "Defensive"):
+                self.evil.OnReboot()
+            elif(self.radio_mode.GetStringSelection() == "Offensive"):
+                self.evil.OnOverwrite(event.device)
+            else:
+                self.evil.OnOverwrite(event.device)
+                self.evil.OnReboot()
+        elif(str(event.action) == "change" and str(event.device.device_node[5:7]) == "sr" and self.chkbox_cd.GetValue() == True):
+            if(self.radio_mode.GetStringSelection() == "Defensive"):
+                self.evil.OnReboot()
+            elif(self.radio_mode.GetStringSelection() == "Offensive"):
+                self.evil.OnEject(event.device)
+            else:
+                self.evil.OnEject(event.device)
+                self.evil.OnReboot()
+        else:
+            pass
+
+
+class Evil:
+    def __init__(self, observer):
+        self.obs = observer
+
+    def OnReboot(self):
+        retcode = subprocess.call("reboot", shell = True)
+
+    def OnOverwrite(self, device):
+        dev = str(device.device_node)
+        retcode = subprocess.call("cat /dev/urandom > " + dev, shell = True)
+
+    def OnEject(self, device):
+        dev = str(device.device_node)
+        retcode = subprocess.call("eject -v " + dev, shell = True)
+
+
+class Porcupine_app(wx.App):
+    def OnInit(self):
+        Porcupine_main()
+        return True
+
+app = Porcupine_app(0)
+app.MainLoop()
---
+++ Readme.md
@@ -0,0 +1,55 @@
+Porcupine:
+==========
+
+Software to prevent unauthorized physical access to your machine.<br>
+It listens for udev events and could take 3 different actions when certain storage devices are detected:<br>
+- 1 - Defense (Reboot PC)<br>
+- 2 - Offense (If it's a USB drive, it'll be overwritten, if it's a CD/DVD rom it'll be eject)<br>
+- 3 - Offense + Defense (The previus modes, combined)<br>
+
+I suggest you (it's quite a must...) to use Full Disk Encryption in order to obtain the best results<br>
+(e.g. What happens if someone try to stick his USB pendrive in your PC? In defensive mode, the PC will reboot and<br> 
+the intruder will be redirected to FDE password prompt)<br>
+
+This program is created with int0x80 talk "Ant-Forensic for the Louise" from Derbycon 2012 in mind.<br>
+For every reference about the original concept by int0x80 please look at:<br>
+- GitHub  - https://github.com/int0x80/anti-forensics<br>
+- Youtube - "Anti-Forensic for the Louise" http://www.youtube.com/watch?v=-HK1JHR7LIM<br>
+- Youtube - "Moar Anti-Forensic for the Louise" http://www.youtube.com/watch?v=i3nLrJrkYOc<br>
+
+
+Requirements:
+=============
+- python >= 2.7<br>
+- python-wxgtk2.8 >= 2.8.12.1<br>
+- pyudev >= 0.16.1<br>
+
+
+Usage:
+======
+
+Simply run it with superadmin privileges<br>
+
+
+ToDo:
+=====
+
+- Add MMC/SD/others controls<br>
+- Add a checksum system for personal pendrives<br>
+- Add the options to run default actions when device is detected<br>
+- Add Shortcut and menu option to hide the trayicon<br>
+- Password protect Quit action (maybe)<br>
+- Password protect Settings menu (maybe)<br>
+- Password protect Stop action (maybe)<br>
+- Password protect Show trayicon action (maybe)<br>
+- Fill or remove the about messagebox<br>
+- Fix tray icon<br>
+- Clean and revise code<br>
+
+- More Tests
+ 
+
+Known bugs:
+===========
+
+- Sometimes the tray icon is displayed wrong<br>
